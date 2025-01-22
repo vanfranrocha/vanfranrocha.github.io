9818af05ac81e3e4d19b07c2f868c675ecc66732
Vanessa <vanfranrocha@hotmail.com>
2025-01-22 11:49:10 -0300

    style<index.html>: adicionado secao de carreira

--- js/custom.js
+++ js/custom.js
@@ -10,3 +10,35 @@ $(document).ready(function(){
 		$grid.masonry();
 	});
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener('click', function(e) {
+		e.preventDefault();
+		// Rola para o elemento alvo
+		document.querySelector(this.getAttribute('href')).scrollIntoView({
+			behavior: 'smooth'
+		});
+	});
+});
+
+const sections = document.querySelectorAll('section');
+const menuLinks = document.querySelectorAll('nav ul li a');
+
+window.addEventListener('scroll', () => {
+	let current = '';
+	
+	sections.forEach(section => {
+		const sectionTop = section.offsetTop;
+		const sectionHeight = section.offsetHeight;
+		if (pageYOffset >= sectionTop - sectionHeight / 3) {
+			current = section.getAttribute('id');
+		}
+	});
+
+	menuLinks.forEach(link => {
+		link.classList.remove('active');
+		if (link.getAttribute('href') === `#${current}`) {
+			link.classList.add('active');
+		}
+	});
+})
