16502f49da8d277c5ed8982c2d51dc22cf4b225c
Vanessa <vanfranrocha@hotmail.com>
2025-01-23 08:01:30 -0300

    style<index.html>: add skills

--- js/custom.js
+++ js/custom.js
@@ -42,3 +42,89 @@ window.addEventListener('scroll', () => {
 		}
 	});
 })
+
+var TxtType = function(el, toRotate, period) {
+	this.toRotate = toRotate;
+	this.el = el;
+	this.loopNum = 0;
+	this.period = parseInt(period, 10) || 2000;
+	this.txt = '';
+	this.tick();
+	this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+	var i = this.loopNum % this.toRotate.length;
+	var fullTxt = this.toRotate[i];
+  
+	if (this.isDeleting) {
+	  this.txt = fullTxt.substring(0, this.txt.length - 1);
+	} else {
+	  this.txt = fullTxt.substring(0, this.txt.length + 1);
+	}
+  
+	this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+	var that = this;
+	var delta = 200 - Math.random() * 100;
+  
+	if (this.isDeleting) { delta /= 2; }
+  
+	if (!this.isDeleting && this.txt === fullTxt) {
+	  delta = this.period;
+	  this.isDeleting = true;
+	} else if (this.isDeleting && this.txt === '') {
+	  this.isDeleting = false;
+	  this.loopNum++;
+	  delta = 500;
+	}
+  
+	setTimeout(function() {
+	  that.tick();
+	}, delta);
+};
+  
+window.onload = function() {
+	var elements = document.getElementsByClassName('typewrite');
+	for (var i=0; i<elements.length; i++) {
+	  var toRotate = elements[i].getAttribute('data-type');
+	  var period = elements[i].getAttribute('data-period');
+	  if (toRotate) {
+		new TxtType(elements[i], JSON.parse(toRotate), period);
+	  }
+	}
+	// INJECT CSS
+	var css = document.createElement("style");
+	css.type = "text/css";
+	css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+	document.body.appendChild(css);
+};
+
+
+const honeycomb = document.getElementById('honeycomb');
+const items = honeycomb.querySelectorAll('.honeycomb-item');
+
+honeycomb.addEventListener('mousemove', (e) => {
+	const rect = honeycomb.getBoundingClientRect();
+	const mouseX = e.clientX - rect.left;
+	const mouseY = e.clientY - rect.top;
+
+	items.forEach(item => {
+		const itemRect = item.getBoundingClientRect();
+		const itemX = itemRect.left + itemRect.width / 2 - rect.left;
+		const itemY = itemRect.top + itemRect.height / 2 - rect.top;
+
+		const distance = Math.sqrt(
+			Math.pow(mouseX - itemX, 2) + Math.pow(mouseY - itemY, 2)
+		);
+
+		const scale = Math.max(1, 2 - distance / 100);
+		item.style.transform = `scale(${scale})`;
+	});
+});
+
+honeycomb.addEventListener('mouseleave', () => {
+	items.forEach(item => {
+		item.style.transform = 'scale(1)';
+	});
+});
